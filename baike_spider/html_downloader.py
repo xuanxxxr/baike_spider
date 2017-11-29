1dc95df114f19807d77f0cd9d4b8568631be01de
轩任 <xuanren@xuanrendeMacBook-Pro.local>
2017-11-29 09:14:56 +0800

    百度百科_Python条目

---
+++ baike_spider/html_downloader.py
@@ -0,0 +1,16 @@
+
+import urllib.request
+
+class HtmlDownloader(object):
+
+
+    def download(self, url):
+        if url is None:
+            return None
+
+        response = urllib.request.urlopen(url)
+
+        if response.getcode() != 200:
+            return None
+
+        return response.read().decode('utf-8')
